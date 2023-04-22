33895dd47ce8d8f9f207f36ccc188efd6f1a9ded
-Maksim- <127635003+Maksim-Yeager@users.noreply.github.com>
2023-04-22 16:43:40 +0300

    timerZone

--- assets/countdown.js
+++ assets/countdown.js
@@ -1,4 +1,4 @@
-function startTimer()
+function startTimer() 
 {
   let remainingTime = 10;
   let displayElement = document.getElementById("timer");
@@ -6,36 +6,36 @@ function startTimer()
 
   const button = document.querySelector('.button-timer');
   button.classList.toggle('hide');
+  const imageClock = document.querySelector('.timer-img');
+  imageClock.classList.toggle('show');
+  const imageTime = document.querySelector('.time-up-img')
+  imageTime.classList.toggle('hide');
 
-  function updateDisplay()
+  function updateDisplay() 
   {
     let minutes = Math.floor(remainingTime / 60);
     let seconds = remainingTime % 60;
     displayElement.textContent = `${minutes.toString().padStart(2,`0`)}:${seconds.toString().padStart(2,`0`)}`;
-
-    if(!document.body.contains(displayElement))
-    {
+    
+    if(!document.body.contains(displayElement)) {
       document.body.appendChild(displayElement);
     }
   }
 
-  function updateTimer()
+  function updateTimer() 
   {
     remainingTime--;
-    
     updateDisplay();
-    
-    if(remainingTime === 0)
-    {
-      const button_show = document.querySelector('.button-timer');
-      //Your mistake are here.
-      button_show.classList.toggle('show');
-      //This line is wrong.
+
+    if(remainingTime === 0) {
       clearInterval(intervalID);
+      imageClock.classList.toggle('show');
+      imageTime.classList.toggle('hide');
+      button.classList.toggle('hide');
     }
   }
 
-  intervalID = setInterval(updateTimer,1000);
+  intervalID = setInterval(updateTimer, 1000);
 }
 
 startTimer();

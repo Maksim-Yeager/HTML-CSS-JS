66e54ba2a775310c7c1da713e45aa59b71efd3a4
Maksim-Yeager <maksim12345daniel@gmail.com>
2023-04-27 00:08:08 +0300

    finish

--- assets/countdown.js
+++ assets/countdown.js
@@ -1,44 +1,53 @@
-function startTimer() 
+let remainingTime = 10;
+let displayElement = document.getElementById("timer");
+let intervalID;
+
+
+const imageClock = document.querySelector('.timer-img');
+imageClock.classList.toggle('show');
+const imageTime = document.querySelector('.time-up-img')
+imageTime.classList.toggle('hide');
+
+function updateDisplay() 
 {
-  let remainingTime = 10;
-  let displayElement = document.getElementById("timer");
-  let intervalID;
-
-  const button = document.querySelector('.button-timer');
-  button.classList.toggle('hide');
-  const imageClock = document.querySelector('.timer-img');
-  imageClock.classList.toggle('show');
-  const imageTime = document.querySelector('.time-up-img')
-  imageTime.classList.toggle('hide');
-
-  function updateDisplay() 
-  {
-    let minutes = Math.floor(remainingTime / 60);
-    let seconds = remainingTime % 60;
-    displayElement.textContent = `${minutes.toString().padStart(2,`0`)}:${seconds.toString().padStart(2,`0`)}`;
+  let minutes = Math.floor(remainingTime / 60);
+  let seconds = remainingTime % 60;
+  displayElement.textContent = `${minutes.toString().padStart(2,`0`)}:${seconds.toString().padStart(2,`0`)}`;
     
-    if(!document.body.contains(displayElement))
-    {
-      document.body.appendChild(displayElement);
-    }
-  }
-
-  function updateTimer() 
+  if(!document.body.contains(displayElement))
   {
-    remainingTime--;
-    updateDisplay();
-
-    if(remainingTime === 0)
-    {
-      clearInterval(intervalID);
-      
-      imageClock.classList.toggle('show');
-      imageTime.classList.toggle('hide');
-      button.classList.toggle('hide');
-    }
+    document.body.appendChild(displayElement);
   }
+}
 
+function updateTimer() 
+{
+  remainingTime--;
+  updateDisplay();
+
+  if(remainingTime === 0)
+  {
+    clearInterval(intervalID);
+      
+    imageClock.classList.toggle('show');
+    imageTime.classList.toggle('hide');
+  }
+}
+
+function startTimer()
+{
+  clearInterval(intervalID);
+  if(remainingTime === 0)
+  {
+    clearInterval(intervalID);
+      
+    imageClock.classList.toggle('show');
+    imageTime.classList.toggle('hide');
+  }
+  remainingTime = 10;
+  updateDisplay();
   intervalID = setInterval(updateTimer, 1000);
 }
 
-startTimer();
+updateDisplay();
+intervalID = setInterval(updateTimer, 1000);
